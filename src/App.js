9cece3dc5e216b6fd958537d73481162cca808e6
Jasonth47 <jasonthowell01@gmail.com>
2023-03-23 23:02:02 -0400

    3

--- src/App.js
+++ src/App.js
@@ -44,34 +44,41 @@ function App() {
   const [showFinalResults, setFinalResults] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
 
-  const renderInput = (type, options) => {
+  const RenderInput = (props) => {
+    const { type, options, onAnswer } = props;
+    const handleAnswerChange = (event) => {
+      console.log(event);
+      onAnswer(event.target.value);
+    };
     if (type === "text") {
       return <input type="text"></input>;
     } else if (type === "multiple-choice") {
       return (
-        <ul>
-          {questions[currentQuestion].options.map((option, index) => {
+        <div>
+          {options.map((option, index) => {
             return (
-              <div>
+              <div key={index}>
                 <label>
-                  <input type="radio" name="answers" value={option.text} />
+                  <input
+                    type="radio"
+                    name="answers"
+                    value={option.text}
+                    onChange={handleAnswerChange}
+                  />
                   {option.text}
                 </label>
               </div>
             );
           })}
-        </ul>
+        </div>
       );
     }
   };
 
-  const nextQuestion = () => {
-    setCurrentQuestion(currentQuestion + 1);
-  };
-
-  const optionClicked = (questionScore) => {
-    setScore(score + questionScore);
+  const nextQuestion = (questionScore) => {
+    // setScore(score + questionScore);
 
     if (currentQuestion + 1 < questions.length) {
       setCurrentQuestion(currentQuestion + 1);
@@ -79,7 +86,12 @@ function App() {
       setFinalResults(true);
     }
   };
-  console.log(score);
+
+  const handleAnswer = (answer) => {
+    const newAnswers = [...answers];
+    newAnswers.push(answer);
+    setAnswers(newAnswers);
+  };
 
   return (
     <div className="App">
@@ -97,12 +109,17 @@ function App() {
             Question {currentQuestion + 1} out of {questions.length}
           </h2>
           <h3 className="question-text">{questions[currentQuestion].text}</h3>
-          {renderInput(
+          {/* {renderInput(
             questions[currentQuestion].type,
             questions[currentQuestion].options
-          )}
+          )} */}
+          <RenderInput
+            type={questions[currentQuestion].type}
+            options={questions[currentQuestion].options}
+            onAnswer={handleAnswer}
+          />
           <br></br>
-          <button onClick={nextQuestion}>Next</button>
+          <button onClick={() => nextQuestion()}>Next</button>
         </div>
       )}
     </div>
