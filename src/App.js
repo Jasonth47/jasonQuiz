fe10e6b0b8d23edef45073a551c3f243e5db1a12
Jasonth47 <jasonthowell01@gmail.com>
2023-03-28 20:49:26 -0400

    slight style

--- src/App.js
+++ src/App.js
@@ -1,64 +1,76 @@
 import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import "./App.css";
 
 const questions = [
-  { text: "what is you name?", type: "text", options: [] },
   {
+    id: "name",
+    text: "what is you name?",
+    type: "text",
+  },
+  {
+    id: "steak",
     text: "How would you order a steak?",
     type: "multiple-choice",
     options: [
-      { id: 0, text: "Well-done", score: -2 },
-      { id: 1, text: "Medium", score: 1 },
-      { id: 2, text: "Medium-Rare", score: 2 },
-      { id: 3, text: "Rare", score: 1 },
-      { id: 4, text: "Im Vegan", score: 1 },
+      { text: "Well-done", score: -2 },
+      { text: "Medium", score: 1 },
+      { text: "Medium-Rare", score: 2 },
+      { text: "Rare", score: 1 },
+      { text: "Im Vegan", score: 1 },
     ],
   },
   {
+    id: "boyChoice",
     text: "question?",
     type: "multiple-choice",
     options: [
-      { id: 0, text: "boy", score: 1 },
-      { id: 1, text: "boy2", score: 2 },
-      { id: 2, text: "boy3", score: 3 },
-      { id: 3, text: "boy4", score: 4 },
+      { text: "boy", score: 1 },
+      { text: "boy2", score: 2 },
+      { text: "boy3", score: 3 },
+      { text: "boy4", score: 4 },
     ],
   },
   {
+    id: "girlChoice",
     text: "question2",
     type: "multiple-choice",
     options: [
-      { id: 0, text: "girl", score: 1 },
-      { id: 1, text: "girl2", score: 2 },
-      { id: 2, text: "girl3", score: 3 },
-      { id: 3, text: "girl4", score: 4 },
+      { text: "girl", score: 1 },
+      { text: "girl2", score: 2 },
+      { text: "girl3", score: 3 },
+      { text: "girl4", score: 4 },
     ],
   },
-  // {
-  //   text: "question3",
-  //   options: [9, 10, 11, 12],
-  // },
 ];
 
-const RenderInput = (props) => {
-  const { type, options, onAnswer } = props;
+const Question = (props) => {
+  const { question, onAnswer } = props;
+  const { type, options, id: questionId } = question;
+
   const handleAnswerChange = (event) => {
-    console.log(event);
-    onAnswer(event.target.value);
+    onAnswer(event.target.name, event.target.value);
   };
+
   if (type === "text") {
-    return <input type="text"></input>;
+    return (
+      <input
+        type="text"
+        name={questionId}
+        onChange={handleAnswerChange}
+      ></input>
+    );
   } else if (type === "multiple-choice") {
     return (
       <div>
         {options.map((option, index) => {
           return (
-            <div key={index}>
+            <div key={index} className="choices">
               <label>
                 <input
                   type="radio"
-                  name="answers"
-                  value={option.text}
+                  name={questionId}
+                  value={option.score}
                   onChange={handleAnswerChange}
                 />
                 {option.text}
@@ -71,15 +83,30 @@ const RenderInput = (props) => {
   }
 };
 
+const Results = (props) => {
+  const { answers } = props;
+  let finalScore = 0;
+
+  for (const key in answers) {
+    const answer = parseInt(answers[key]);
+    if (!isNaN(answer)) {
+      finalScore += answer;
+    }
+  }
+
+  return (
+    <h2>
+      You Finished, {answers.name}! Your final score is {finalScore}.
+    </h2>
+  );
+};
+
 function App() {
   const [showFinalResults, setFinalResults] = useState(false);
-  const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answers, setAnswers] = useState([]);
+  const [answers, setAnswers] = useState({});
 
-  const nextQuestion = (questionScore) => {
-    // setScore(score + questionScore);
-
+  const nextQuestion = () => {
     if (currentQuestion + 1 < questions.length) {
       setCurrentQuestion(currentQuestion + 1);
     } else {
@@ -87,39 +114,30 @@ function App() {
     }
   };
 
-  const handleAnswer = (answer) => {
-    const newAnswers = [...answers];
-    newAnswers.push(answer);
+  const handleAnswer = (questionId, answer) => {
+    const newAnswers = { ...answers, [questionId]: answer };
     setAnswers(newAnswers);
   };
 
   return (
-    <div className="App">
+    <div className="app">
       <h1>would we be friends</h1>
-      <h2>score:{score}</h2>
+      <h2>Answers:{JSON.stringify(answers)}</h2>
 
       {showFinalResults ? (
-        <div clasName="final-results">
-          <h2>Nick</h2>
-          <button></button>
-        </div>
+        <Results answers={answers} />
       ) : (
         <div className="question-card">
-          <h2>
+          <h2 className="quest-count">
             Question {currentQuestion + 1} out of {questions.length}
           </h2>
           <h3 className="question-text">{questions[currentQuestion].text}</h3>
-          {/* {renderInput(
-            questions[currentQuestion].type,
-            questions[currentQuestion].options
-          )} */}
-          <RenderInput
-            type={questions[currentQuestion].type}
-            options={questions[currentQuestion].options}
+          <Question
+            question={questions[currentQuestion]}
             onAnswer={handleAnswer}
           />
           <br></br>
-          <button onClick={() => nextQuestion()}>Next</button>
+          <button onClick={nextQuestion}>Next</button>
         </div>
       )}
     </div>
